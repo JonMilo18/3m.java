f4369b30e40e6f9d76ff18f0c23270373616b3ed
Iraitz Moran <ira_mor_fer@goierrieskola.org>
2024-04-25 12:13:27 +0200

    Alten errorea komponduta

--- 3m/src/main/AltakAdmin.java
+++ 3m/src/main/AltakAdmin.java
@@ -9,10 +9,15 @@ import java.awt.Color;
 import javax.swing.JTabbedPane;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 
 public class AltakAdmin extends JFrame {

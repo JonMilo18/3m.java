21a8d6fa82e16d60584765f52b7cfea733b870e9
Jon-Milo <Jonmilo80@gmail.com>
2024-04-25 17:43:09 +0200

    MenuB funtzionalitatea

--- 3m/src/main/MenuB.java
+++ 3m/src/main/MenuB.java
@@ -7,6 +7,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MenuB extends JFrame {
 
@@ -42,16 +44,34 @@ public class MenuB extends JFrame {
 		contentPane.setLayout(null);
 		
 		JButton btnNewButton = new JButton("Altak");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AltakAdmin oforma = new AltakAdmin();
+                oforma.setVisible(true);
+			}
+		});
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		btnNewButton.setBounds(0, 0, 485, 308);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Aldakeak");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AldaketakAdmin oforma = new AldaketakAdmin();
+                oforma.setVisible(true);
+			}
+		});
 		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		btnNewButton_1.setBounds(446, 0, 504, 308);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Kontsultak");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				KontsultakAdmin oforma = new KontsultakAdmin();
+                oforma.setVisible(true);
+			}
+		});
 		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		btnNewButton_2.setBounds(0, 305, 950, 294);
 		contentPane.add(btnNewButton_2);
